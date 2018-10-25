646da81dd7f61fe9feb169f030e8a978417d4877
Hamed Naseri <41665178+hamednasery@users.noreply.github.com>
2018-10-25 15:17:48 +0330

    Add files via upload

---
+++ Howsweet/Howsweet/firstpack/script/controller.js
@@ -0,0 +1,23 @@
+﻿angular.module('umbraco').controller('departmentedit', function ($scope, $routeParams, departmentResource, notificationsService) {
+    $scope.loaded = false;
+
+    if ($routeParams.id == -1) {
+                 $scope.department = {};
+                 $scope.loaded = true;
+    }
+    else{
+                     //get a person id -> service
+                     departmentResource.getById($routeParams.id).then(function (response) {
+                     $scope.department = response.data;
+                     $scope.loaded = true;
+                     });
+    }
+
+    $scope.save = function (department) {
+              departmentResource.save(department).then(function (response) {
+                  $scope.department = response.data;
+            
+                  notificationsService.success("عملیات با موفقیت", department.name + " ذخیره شد");
+                 });
+    };
+});
